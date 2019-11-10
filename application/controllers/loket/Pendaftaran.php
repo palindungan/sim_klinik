2dbced7286edff1bca30339de1c25fad72031b9f
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-11-11 02:58:44 +0700

    partials
    partials

---
+++ application/controllers/loket/Pendaftaran.php
@@ -0,0 +1,14 @@
+<?php
+class Pendaftaran extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('loket/M_pendaftaran');
+    }
+    public function index()
+    {
+        $this->template->load('sim_klinik/template/loket', 'sim_klinik/konten/loket/pendaftaran/tampil');
+    }
+}
