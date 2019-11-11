56d414d88e5aa783d82562c72c6cda97bdb12368
zalramli <github.little@gmail.com>
2019-11-11 16:57:57 +0700

    fix validation

--- application/controllers/loket/Pendaftaran.php
+++ application/controllers/loket/Pendaftaran.php
@@ -15,4 +15,33 @@ class Pendaftaran extends CI_Controller
     {
         $this->template->load('sim_klinik/template/loket','sim_klinik/konten/loket/pendaftaran/tambah');
     }
+    
+    public function store()
+    {
+        $this->form_validation->set_rules('no_ref','No. Ref','required');
+        $this->form_validation->set_rules('no_rm','No. RM','required');
+        $this->form_validation->set_rules('nik','NIK','required|numeric|min_length[16]|max_length[16]');
+        $this->form_validation->set_rules('tempat_lahir','tempat lahir','required|alpha');
+        $this->form_validation->set_rules('alamat','alamat','required');
+        date_default_timezone_set("Asia/Jakarta");
+        $now = Date('Y-m-d');
+        $hari = $this->input->post('hari');
+        $bulan = $this->input->post('bulan');
+        $tahun = $this->input->post('tahun');
+        $tgl_lahir = $tahun."-".$bulan."-".$hari;
+        $lama = selisihHari($tgl_lahir,$now);
+        $this->form_validation->set_message('cek_umur', 'Umur pasien kurang dari 1 hari');
+        if($lama < 0)
+        {
+            $this->form_validation->set_rules('hari','Error aja','cek_umur');
+        }
+        if ($this->form_validation->run() == FALSE)
+        {
+            $this->template->load('sim_klinik/template/loket','sim_klinik/konten/loket/pendaftaran/tambah');
+        }
+        else
+        {
+            echo $lama;
+        }
+    }
 }
--- application/helpers/custom_helper.php
+++ application/helpers/custom_helper.php
@@ -2,7 +2,16 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 if (!function_exists('rupiah')) {
     function rupiah($angka){
-    $hasil_rupiah = number_format($angka,0,',','.');
-    return $hasil_rupiah;
+        $hasil_rupiah = number_format($angka,0,',','.');
+        return $hasil_rupiah;
+    }
+    function selisihHari($CheckIn,$CheckOut){
+        $CheckInX = explode("-", $CheckIn);
+        $CheckOutX = explode("-", $CheckOut);
+        $date1 = mktime(0, 0, 0, $CheckInX[1],$CheckInX[2],$CheckInX[0]);
+        $date2 = mktime(0, 0, 0, $CheckOutX[1],$CheckOutX[2],$CheckOutX[0]);
+        $interval =($date2 - $date1)/(3600*24);
+        // returns numberofdays
+        return $interval ;
     }
 }
