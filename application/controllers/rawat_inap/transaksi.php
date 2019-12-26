e14d6179fadfb6d96521f945c70f3aa8a09a40b3
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-12-27 06:27:50 +0700

    proses rawat inap
    proses rawat inap

--- application/controllers/rawat_inap/transaksi.php
+++ application/controllers/rawat_inap/transaksi.php
@@ -6,9 +6,37 @@ class Transaksi extends CI_Controller
         parent::__construct();
         $this->load->model('rawat_inap/M_transaksi');
     }
+
     public function index()
     {
         $data['record'] = $this->M_transaksi->tampil_data('data_pelayanan_pasien')->result();
         $this->template->load('sim_klinik/template/rawat_inap', 'sim_klinik/konten/rawat_inap/transaksi/tambah', $data);
+    }
+
+    public function tampil_daftar_kamar()
+    {
+        $data_tbl['tbl_data'] = $this->M_transaksi->tampil_data('kamar_rawat_inap')->result();
+
+        $data = json_encode($data_tbl);
+
+        echo $data;
+    }
+
+    public function tampil_daftar_tindakan()
+    {
+        $data_tbl['tbl_data'] = $this->M_transaksi->tampil_data('rawat_inap_tindakan')->result();
+
+        $data = json_encode($data_tbl);
+
+        echo $data;
+    }
+
+    public function tampil_daftar_obat()
+    {
+        $data_tbl['tbl_data'] = $this->M_transaksi->tampil_data('daftar_pengiriman_obat_apotek_detail')->result();
+
+        $data = json_encode($data_tbl);
+
+        echo $data;
     }
 }
--- application/views/sim_klinik/konten/rawat_inap/transaksi/tambah.php
+++ application/views/sim_klinik/konten/rawat_inap/transaksi/tambah.php
@@ -9,9 +9,16 @@
         <div class="card-body">
             <form method="post" id="transaksi_form">
                 <div class="form-row">
-                    <div class="form-group col-sm-4">
-                        <label for="">No Ref Pelayanan</label>
-                        <input type="text" name="no_ref_pelayanan" class="form-control form-control-sm no_refnya" required>
+                    <div class="form-group col-sm-6">
+                        <label>Cari No Ref</label>
+                        <select class="form-control form-control-sm" name="no_ref_pelayanan" id="xx" required>
+                            <option value="">-- Pilih Data --</option>
+                            <?php foreach ($record as $data) : ?>
+                                <option value="<?= $data->no_ref_pelayanan ?>">
+                                    <?= $data->no_ref_pelayanan . " || " . $data->nama ?>
+                                </option>
+                            <?php endforeach; ?>
+                        </select>
                     </div>
                 </div>
 
@@ -63,7 +70,7 @@
                             <span class="icon text-white-50">
                                 <i class="fas fa-search-plus"></i>
                             </span>
-                            <span class="text">Cari Tindakan</span>
+                            <span class="text">Cari Obat</span>
                         </a>
                     </div>
 
@@ -111,7 +118,7 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Daftar Checkup</h5>
+                <h5 class="modal-title" id="exampleModalLongTitle">Daftar Obat Rawat Inap</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -121,10 +128,13 @@
                     <table class="table table-bordered table_1" width="100%" cellspacing="0">
                         <thead>
                             <tr>
-                                <th class="text-center">No</th>
-                                <th class="text-center">Nama</th>
-                                <th class="text-center">Biaya</th>
-                                <th class="text-center">Aksi</th>
+                                <th>No</th>
+                                <th>Nama Obat</th>
+                                <th>Kategori</th>
+                                <th>Tanggal Transfer</th>
+                                <th>Stok Saat Ini</th>
+                                <th>Harga Jual</th>
+                                <th>Aksi</th>
                             </tr>
                         </thead>
                         <tbody id="daftar_barang">
@@ -139,152 +149,70 @@
 <script src="<?= base_url(); ?>assets/sb_admin_2/vendor/jquery/jquery-3.4.1.min.js"></script>
 
 <script>
-    $(document).ready(function() {
-        auto_complete();
-
-    });
-
-    function auto_complete() {
-        $(function() {
-            $(".no_refnya").autocomplete({
-                source: function(request, response) {
-                    // Fetch data
-                    $.ajax({
-                        url: "<?php echo base_url() . 'laboratorium/transaksi/get_autocomplete'; ?>",
-                        type: 'post',
-                        dataType: "json",
-                        data: {
-                            nilai: request.term
-                        },
-                        success: function(data) {
-                            response(data);
-                        }
-                    });
-                },
-                select: function(event, ui) {},
-            });
-
-            $(".no_refnya").on("autocompleteselect", function(event, ui) {
-
-                var id = ui.item.value
-
-                // Fetch data
-                $.ajax({
-                    url: "<?php echo base_url() . 'laboratorium/transaksi/get_pasien_by_no_ref_pelayanan'; ?>",
-                    type: 'post',
-                    data: {
-                        nilai: id
-                    },
-                    success: function(hasil) {
-
-                        // parse
-                        var obj = JSON.parse(hasil);
-                        let data = obj['tbl_data'];
-
-                        if (data != '') {
-
-                            $.each(data, function(i, item) {
-
-                                var nama = data[i].nama;
-                                var alamat = data[i].alamat;
-                                var tgl_lahir = data[i].tgl_lahir;
-
-                                // parse
-                                var tahun = tgl_lahir.substring(0, 4);
-
-                                var date = new Date();
-                                var year = date.getFullYear();
-
-                                var umur = year - tahun;
-
-                                $("#txt_nama").html(nama);
-                                $("#txt_alamat").html(alamat);
-                                $("#txt_umur").html(umur);
-                            });
-                        } else {
-
-                            alert("Data Dengan Kode : " + id + " Tidak Ditemukan !");
-
-                        }
-                    }
-                });
-            });
-
-            $(".no_refnya").on("autocompletesearch", function(event, ui) {
-                $("#txt_nama").html("");
-                $("#txt_alamat").html("");
-                $("#txt_umur").html("");
-            });
-        });
-    }
-</script>
-
-<script>
     var count1 = 0;
     var jumlah_detail_transaksi = 0;
+
+    $(document).on('change', '#xx', function(event) {
+        var nilai_value = $('#xx').val();
+
+        // Fetch data
+        $.ajax({
+            url: "<?php echo base_url() . 'apotek/penjualan_obat/get_pasien_by_no_ref_pelayanan'; ?>",
+            type: 'post',
+            data: {
+                nilai: nilai_value
+            },
+            success: function(hasil) {
+
+                // parse
+                var obj = JSON.parse(hasil);
+                let data = obj['tbl_data'];
+
+                if (data != '') {
+
+                    $.each(data, function(i, item) {
+
+                        var nama = data[i].nama;
+                        var alamat = data[i].alamat;
+                        var tgl_lahir = data[i].tgl_lahir;
+
+                        // parse
+                        var tahun = tgl_lahir.substring(0, 4);
+
+                        var date = new Date();
+                        var year = date.getFullYear();
+
+                        var umur = year - tahun;
+
+                        $("#txt_nama").html(nama);
+                        $("#txt_alamat").html(alamat);
+                        $("#txt_umur").html(umur);
+                    });
+                } else {
+
+                    alert("Data Dengan Kode : " + nilai_value + " Tidak Ditemukan !");
+
+                }
+            }
+        });
+    });
+
+    // start of tambah obat
 
     // jika kita tekan / click button search-button
     $('#btn_search').on('click', function() {
         search_proses();
     });
 
-    // jika kita tekan hapus / click button
-    $(document).on('click', '.remove_baris', function() {
-        var row_no = $(this).attr("id");
-        $('#row' + row_no).remove();
-
-        jumlah_detail_transaksi = jumlah_detail_transaksi - 1;
-
-        cekJumlahDataTransaksi();
-    });
-
-    // jika kita mengubah class inputan rupiah
-    $(document).on('keyup', '.rupiah', function() {
-        update_total();
-    });
-
-    // jika di click simpan / submit
-    $(document).on('submit', '#transaksi_form', function(event) {
-        event.preventDefault();
-
-        // mengambil nilai di dalam form
-        var form_data = $(this).serialize();
-
-        // tambah ke database
-        $.ajax({
-            url: "<?php echo base_url() . 'laboratorium/transaksi/input_transaksi_form'; ?>",
-            method: "POST",
-            data: form_data,
-            success: function(data) {
-                alert(data);
-                location.reload();
-            }
-        });
-        // tambah ke database
-
-    });
-
-    // Start pencarian
     function search_proses() {
 
         var table;
-        table = $('.table_1').DataTable({
-            "columnDefs": [{
-                    "targets": [0, 3],
-                    "className": "text-center"
-                },
-                {
-                    "targets": 2,
-                    "className": "text-right"
-                }
-            ],
-            "bDestroy": true
-        });
+        table = $('.table_1').DataTable();
 
         table.clear();
 
         $.ajax({
-            url: "<?php echo base_url() . 'laboratorium/transaksi/tampil_daftar_checkup'; ?>",
+            url: "<?php echo base_url() . 'rawat_inap/transaksi/tampil_daftar_obat'; ?>",
             success: function(hasil) {
 
                 var obj = JSON.parse(hasil);
@@ -296,17 +224,20 @@
 
                     $.each(data, function(i, item) {
 
-                        var kode = data[i].no_lab_c;
-                        var nama = data[i].nama;
-                        var harga = data[i].harga;
-                        var reverse = harga.toString().split('').reverse().join(''),
-                            ribuan = reverse.match(/\d{1,3}/g);
-                        ribuan = ribuan.join('.').split('').reverse().join('');
-                        var button = `<a onclick="pilihTindakan('` + kode +
-                            `','` + nama + `','` + harga + `')" id="` + kode +
+                        var kode = data[i].no_stok_obat_rawat_i;
+                        var nama = data[i].nama_obat;
+                        var nama_kategori = data[i].nama_kategori;
+                        var tgl_obat_keluar_i = data[i].tgl_obat_keluar_i;
+                        var qty_sekarang = data[i].qty_sekarang;
+                        var harga_jual = data[i].harga_jual;
+
+                        var button = `<a onclick="pilihObat('` + kode +
+                            `','` + nama + `','` + harga_jual + `','` + qty_sekarang + `')" id="` + kode +
                             `" class="btn btn-sm btn-dark text-white">Pilih</a>`;
 
-                        table.row.add([no, nama, ribuan, button]);
+                        table.row.add([no, nama, nama_kategori, tgl_obat_keluar_i, qty_sekarang,
+                            harga_jual, button
+                        ]);
 
                         no = no + 1;
                     });
@@ -322,7 +253,7 @@
     }
 
     // Start add_row
-    function pilihTindakan(kode, nama, harga) {
+    function pilihObat(kode, nama, harga_jual, qty_sekarang) {
 
         $('#detail_list').append(`
 
@@ -330,12 +261,18 @@
 				<div class="form-group col-sm-5">
 					<input type="text" readonly name="nama[]" class="form-control form-control-sm karakter" id="nama` + count1 +
             `" placeholder="Nama" required value="` + nama + `">
-					<input type="hidden" name="no_lab_c[]" class="form-control form-control-sm" id="no_lab_c` + count1 + `" value="` +
+					<input type="hidden" name="no_stok_obat_a[]" class="form-control form-control-sm" id="no_stok_obat_a` + count1 +
+            `" value="` +
             kode + `">
 				</div>
-				<div class="form-group col-sm-5">
-					<input type="text" name="harga[]" class="form-control form-control-sm rupiah text-right" id="harga` + count1 +
-            `" placeholder="Harga" required value="` + harga + `">
+				<div class="form-group col-sm-4">
+					<input type="text" name="harga_jual[]" class="form-control form-control-sm rupiah text-right" id="harga_jual` +
+            count1 +
+            `" placeholder="harga supplier" required value="` + harga_jual + `">
+				</div>
+                <div class="form-group col-sm-1">
+					<input type="text" name="qty[]" class="form-control form-control-sm rupiah" id="qty` + count1 + `" placeholder="QTY" value="1" required>
+					<input type="hidden" name="qty_sekarang[]" id="qty_sekarang` + count1 + `" class="form-control form-control-sm" value="` + qty_sekarang + `"></input>
 				</div>
 				<div class="form-group col-sm-2">
 					<a id="` + count1 + `" href="#" class="btn btn-sm btn-danger btn-icon-split remove_baris">
@@ -353,10 +290,10 @@
         jumlah_detail_transaksi = jumlah_detail_transaksi + 1;
         $('#exampleModalCenter').modal('hide');
 
-        cekJumlahDataTransaksi();
+        cekJumlahDataDetailTransaksi();
     }
 
-    function cekJumlahDataTransaksi() {
+    function cekJumlahDataDetailTransaksi() {
 
         var x = document.getElementById("label_kosong");
         if (jumlah_detail_transaksi > 0) {
@@ -373,7 +310,7 @@
         var form_data = $('#transaksi_form').serialize()
 
         $.ajax({
-            url: "<?php echo base_url() . 'laboratorium/transaksi/ambil_total'; ?>",
+            url: "<?php echo base_url() . 'apotek/penjualan_obat/ambil_total'; ?>",
             method: "POST",
             data: form_data,
             success: function(data) {
@@ -384,6 +321,59 @@
 
         validasi();
     }
+
+    // jika kita tekan hapus / click button
+    $(document).on('click', '.remove_baris', function() {
+        var row_no = $(this).attr("id");
+        $('#row' + row_no).remove();
+
+        jumlah_detail_transaksi = jumlah_detail_transaksi - 1;
+
+        cekJumlahDataDetailTransaksi();
+    });
+
+    // end of tambah obat
+
+    // jika kita mengubah class inputan rupiah
+    $(document).on('keyup', '.rupiah', function() {
+
+        var row_id = $(this).attr("id"); // qty1++
+        var row_no = row_id.substring(3); // 1++
+
+        var val_qty = parseInt($('#' + row_id).val());
+        var val_qty_sekarang = parseInt($('#qty_sekarang' + row_no).val());
+
+        if (val_qty <= val_qty_sekarang) {
+            update_total();
+        } else {
+            alert("Maaf Qty Tidak Boleh Detail Obat Melebihi Stok Apotek");
+            $('#' + row_id).val("1");
+            update_total();
+        }
+    });
+
+    // jika di click simpan / submit
+    $(document).on('submit', '#transaksi_form', function(event) {
+        event.preventDefault();
+
+        // mengambil nilai di dalam form
+        var form_data = $(this).serialize();
+
+        // tambah ke database
+        $.ajax({
+            url: "<?php echo base_url() . 'apotek/penjualan_obat/input_transaksi_form'; ?>",
+            method: "POST",
+            data: form_data,
+            success: function(data) {
+                if (data != "") {
+                    alert(data);
+                }
+                location.reload();
+            }
+        });
+        // tambah ke database
+
+    });
 
     function validasi() {
         $('.rupiah').mask('000.000.000', {
