9beeef004a18eb9dce47967b594f88b7c6678983
Mohamad Rizal Ramli <22782888+zalramli@users.noreply.github.com>
2019-12-30 10:30:11 +0700

    Perubahan code no ref
    --

--- application/models/loket/M_pendaftaran.php
+++ application/models/loket/M_pendaftaran.php
@@ -37,18 +37,20 @@ class M_pendaftaran extends CI_Model
         $field = "no_ref_pelayanan";
         $tabel = "pelayanan";
         $digit = "3";
+        $ymd = date('ymd');
 
-        $q = $this->db->query("SELECT MAX(RIGHT($field,$digit)) AS kd_max FROM $tabel");
+        $q = $this->db->query("SELECT MAX(RIGHT($field,$digit)) AS kd_max FROM $tabel WHERE SUBSTR($field, 3, 6) = $ymd LIMIT 1");
         $kd = "";
         if ($q->num_rows() > 0) {
             foreach ($q->result() as $k) {
                 $tmp = ((int) $k->kd_max) + 1;
-                $kd = sprintf('%0' . $digit . 's',  $tmp);
+                $kd = sprintf('%0' . $digit . 's', $tmp);
             }
         } else {
             $kd = "001";
         }
-        return date('ymd') . '-' . $kd;
+        date_default_timezone_set('Asia/Jakarta');
+        return date('ymd') . '-' . $kd; // SELECT SUBSTR('RI191121-0001', 3, 6); dari digit ke 3 sampai 6 digit seanjutnya
     }
 
     function get_no_dewasa_bp()
