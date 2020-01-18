e241716c703f7f4c6fec580f11da07ba18b32d4e
rizkikaZakka <rizkika.palindungan1@gmail.com>
2020-01-19 05:47:56 +0700

    pembenaran baris tagihan apotek obat
    pembenaran baris tagihan apotek obat

--- application/views/sim_klinik/konten/administrasi/tagihan/load_php/apotek_obat.php
+++ application/views/sim_klinik/konten/administrasi/tagihan/load_php/apotek_obat.php
@@ -117,10 +117,11 @@
                     <input type="checkbox" name="status_paket_apotek_obat[]" class="col-md-1 icon deteksi_cek_box form-control form-control-sm" id="status_paket_apotek_obat` + count_transaksi + `" value="Ya">
                     <input type="text" name="harga_apotek_obat[]" class="col-md-11 form-control form-control-sm rupiah text-right harga_apotek_obat_update" id="harga_apotek_obat` + count_transaksi + `" placeholder="Harga Obat Apotek" required value="` + harga_apotek_obat + `">
                 </div>
-                <input type="hidden" class="form-control form-control-sm rupiah text-right" id="harga_cadangan_apotek_obat` + count_transaksi + `" readonly required value="` + harga_apotek_obat + `"></td>
+                <input type="hidden" class="form-control form-control-sm rupiah text-right" id="harga_cadangan_apotek_obat` + count_transaksi + `" readonly required value="` + harga_apotek_obat + `">
             </td>
             <td>  
-                <input type="text" class="form-control form-control-sm rupiah text-right" id="harga_sub_apotek_obat` + count_transaksi + `" readonly required value="` + harga_apotek_obat + `"></td>
+                <input type="text" class="form-control form-control-sm rupiah text-right" id="harga_sub_apotek_obat` + count_transaksi + `" readonly required value="` + harga_apotek_obat + `">
+            </td>
             <td>
                 <div class="form-group col-sm-2">
                     <a id="` + count_transaksi + `" href="#" class="btn btn-sm btn-danger btn-icon-split remove_baris_apotek_obat">
--- application/views/sim_klinik/konten/administrasi/tagihan/load_php/load_all_detail.php
+++ application/views/sim_klinik/konten/administrasi/tagihan/load_php/load_all_detail.php
@@ -225,12 +225,15 @@
                     <input readonly type="hidden" name="cek_qty_apotek_obat[]" class="form-control form-control-sm" id="cek_qty_apotek_obat` + count_transaksi + `" value="` + qty_sekarang + `">
                 </td>
                 <td>
-                    <input type="text" name="harga_apotek_obat[]" class="form-control form-control-sm rupiah text-right harga_apotek_obat_update" id="harga_apotek_obat` + count_transaksi + `" placeholder="Harga Obat Apotek" required value="` + harga_jual + `">
-                    <input type="checkbox" name="status_paket_apotek_obat[]" class="deteksi_cek_box form-control form-control-sm" id="status_paket_apotek_obat` + count_transaksi + `" value="Ya">
-                    <input type="hidden" class="form-control form-control-sm rupiah text-right" id="harga_cadangan_apotek_obat` + count_transaksi + `" readonly required value="` + harga_jual + `"></td>
+                    <div class="btn-icon-split col-12">
+                        <input type="checkbox" name="status_paket_apotek_obat[]" class="col-md-1 icon deteksi_cek_box form-control form-control-sm" id="status_paket_apotek_obat` + count_transaksi + `" value="Ya">
+                        <input type="text" name="harga_apotek_obat[]" class="col-md-11 form-control form-control-sm rupiah text-right harga_apotek_obat_update" id="harga_apotek_obat` + count_transaksi + `" placeholder="Harga Obat Apotek" required value="` + harga_jual + `">
+                    </div>
+                    <input type="hidden" class="form-control form-control-sm rupiah text-right" id="harga_cadangan_apotek_obat` + count_transaksi + `" readonly required value="` + harga_jual + `">
                 </td>
                 <td>  
-                    <input type="text" class="form-control form-control-sm rupiah text-right" id="harga_sub_apotek_obat` + count_transaksi + `" readonly required value="` + harga_jual * qty + `"></td>
+                    <input type="text" class="form-control form-control-sm rupiah text-right" id="harga_sub_apotek_obat` + count_transaksi + `" readonly required value="` + harga_jual * qty + `">
+                </td>
                 <td>
                     <div class="form-group col-sm-2">
                         <a id="` + count_transaksi + `" href="#" class="btn btn-sm btn-danger btn-icon-split remove_baris_apotek_obat">
