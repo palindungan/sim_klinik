d60426e26a7020822db1047f0a306f627e82ee67
rizkikaZakka <rizkika.palindungan1@gmail.com>
2020-01-19 19:14:57 +0700

    view + load kia_penanganan
    view + load kia_penanganan

--- application/views/sim_klinik/konten/administrasi/tagihan/load_php/load_all_detail.php
+++ application/views/sim_klinik/konten/administrasi/tagihan/load_php/load_all_detail.php
@@ -165,9 +165,10 @@
 
                         var no_kia_t = data_kia_tindakan[i].no_kia_t;
                         var nama = data_kia_tindakan[i].nama;
+                        var qty = data_kia_tindakan[i].qty;
                         var harga = data_kia_tindakan[i].harga;
 
-                        load_detail_kia_tindakan(no_kia_t, nama, harga);
+                        load_detail_kia_tindakan(no_kia_t, nama, qty, harga);
                     });
 
                     update_sub_total_kia_tindakan();
@@ -472,7 +473,7 @@
         jumlah_detail_transaksi = jumlah_detail_transaksi + 1;
     }
 
-    function load_detail_kia_tindakan(no_kia_t, nama, harga) {
+    function load_detail_kia_tindakan(no_kia_t, nama, qty, harga) {
 
         $('#detail_list_kia_tindakan').append(`
 
@@ -481,12 +482,14 @@
                 ` + nama + `
                 <input type="hidden" name="no_kia_t[]" class="form-control form-control-sm" id="no_kia_t` + count_transaksi + `" value="` + no_kia_t + `">
             </td>
-            <td>1</td>
+            <td>
+                <input type="text" name="qty_kia_tindakan[]" class="form-control form-control-sm cek_qty_kia_tindakan" id="qty_kia_tindakan` + count_transaksi + `" placeholder="QTY" value="` + qty + `" required>
+            </td>
             <td>
                 <input type="text" name="harga_kia_tindakan[]" class="form-control form-control-sm rupiah text-right harga_kia_tindakan_update" id="harga_kia_tindakan` + count_transaksi + `" placeholder="Harga Tindakan KIA" required value="` + harga + `">
             </td>
             <td>  
-                <input type="text" class="form-control form-control-sm rupiah text-right" id="harga_sub_kia_tindakan` + count_transaksi + `" readonly required value="` + harga + `"></td>
+                <input type="text" class="form-control form-control-sm rupiah text-right" id="harga_sub_kia_tindakan` + count_transaksi + `" readonly required value="` + harga * qty + `"></td>
             <td>
                 <div class="form-group col-sm-2">
                     <a id="` + count_transaksi + `" href="#" class="btn btn-sm btn-danger btn-icon-split remove_baris_kia_tindakan">
