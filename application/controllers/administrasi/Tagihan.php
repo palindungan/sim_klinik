c409d0d97f8df7c803bf443daae83f3bcccdea0d
rizkikaZakka <rizkika.palindungan1@gmail.com>
2020-01-19 19:33:21 +0700

    sub total kia penanganan
    sub total kia penanganan

--- application/controllers/administrasi/Tagihan.php
+++ application/controllers/administrasi/Tagihan.php
@@ -160,14 +160,17 @@ class Tagihan extends CI_Controller
         $sub_total = 0;
         $total = 0;
 
-        if (isset($_POST['no_lab_c']) && isset($_POST['harga_lab_tindakan'])) {
+        if (isset($_POST['no_lab_c']) && isset($_POST['harga_lab_tindakan']) && isset($_POST['qty_lab_tindakan'])) {
 
             for ($i = 0; $i < count($this->input->post('no_lab_c')); $i++) {
 
                 $harga_jual_temp = $this->input->post('harga_lab_tindakan')[$i];
                 $harga_jual = (int) preg_replace("/[^0-9]/", "", $harga_jual_temp);
 
-                $perhitungan = $harga_jual;
+                $qty_temp = $this->input->post('qty_lab_tindakan')[$i];
+                $qty = (int) $qty_temp;
+
+                $perhitungan = $harga_jual * $qty;
 
                 $sub_total = $sub_total + $perhitungan;
             }
