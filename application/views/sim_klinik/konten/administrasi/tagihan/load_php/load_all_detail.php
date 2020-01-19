42671ffad539ccec40b29cf078caad4ca97c6455
rizkikaZakka <rizkika.palindungan1@gmail.com>
2020-01-19 20:37:23 +0700

    4 view + load ugd_penanganan
    4 view + load ugd_penanganan

--- application/views/sim_klinik/konten/administrasi/tagihan/load_php/load_all_detail.php
+++ application/views/sim_klinik/konten/administrasi/tagihan/load_php/load_all_detail.php
@@ -150,9 +150,10 @@
 
                         var no_ugd_t = data_ugd_tindakan[i].no_ugd_t;
                         var nama = data_ugd_tindakan[i].nama;
+                        var qty = data_ugd_tindakan[i].qty;
                         var harga = data_ugd_tindakan[i].harga;
 
-                        load_detail_ugd_tindakan(no_ugd_t, nama, harga);
+                        load_detail_ugd_tindakan(no_ugd_t, nama, qty, harga);
                     });
 
                     update_sub_total_ugd_tindakan();
@@ -444,7 +445,7 @@
         jumlah_detail_transaksi = jumlah_detail_transaksi + 1;
     }
 
-    function load_detail_ugd_tindakan(no_ugd_t, nama, harga) {
+    function load_detail_ugd_tindakan(no_ugd_t, nama, qty, harga) {
 
         $('#detail_list_ugd_tindakan').append(`
 
@@ -453,12 +454,14 @@
                 ` + nama + `
                 <input type="hidden" name="no_ugd_t[]" class="form-control form-control-sm" id="no_ugd_t` + count_transaksi + `" value="` + no_ugd_t + `">
             </td>
-            <td>1</td>
+            <td>
+                <input type="text" name="qty_ugd_tindakan[]" class="form-control form-control-sm cek_qty_ugd_tindakan" id="qty_ugd_tindakan` + count_transaksi + `" placeholder="QTY" value="` + qty + `" required>
+            </td>
             <td>
                 <input type="text" name="harga_ugd_tindakan[]" class="form-control form-control-sm rupiah text-right harga_ugd_tindakan_update" id="harga_ugd_tindakan` + count_transaksi + `" placeholder="Harga Tindakan UGD" required value="` + harga + `">
             </td>
             <td>  
-                <input type="text" class="form-control form-control-sm rupiah text-right" id="harga_sub_ugd_tindakan` + count_transaksi + `" readonly required value="` + harga + `"></td>
+                <input type="text" class="form-control form-control-sm rupiah text-right" id="harga_sub_ugd_tindakan` + count_transaksi + `" readonly required value="` + harga * qty + `"></td>
             <td>
                 <div class="form-group col-sm-2">
                     <a id="` + count_transaksi + `" href="#" class="btn btn-sm btn-danger btn-icon-split remove_baris_ugd_tindakan">
