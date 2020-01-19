d62a948b8614ae21b107a747704fadf15b94862e
rizkikaZakka <rizkika.palindungan1@gmail.com>
2020-01-19 20:29:05 +0700

    sub total ugd penanganan
    sub total ugd penanganan

--- application/controllers/administrasi/Tagihan.php
+++ application/controllers/administrasi/Tagihan.php
@@ -186,14 +186,17 @@ class Tagihan extends CI_Controller
         $sub_total = 0;
         $total = 0;
 
-        if (isset($_POST['no_ugd_t']) && isset($_POST['harga_ugd_tindakan'])) {
+        if (isset($_POST['no_ugd_t']) && isset($_POST['harga_ugd_tindakan']) && isset($_POST['qty_ugd_tindakan'])) {
 
             for ($i = 0; $i < count($this->input->post('no_ugd_t')); $i++) {
 
                 $harga_jual_temp = $this->input->post('harga_ugd_tindakan')[$i];
                 $harga_jual = (int) preg_replace("/[^0-9]/", "", $harga_jual_temp);
 
-                $perhitungan = $harga_jual;
+                $qty_temp = $this->input->post('qty_ugd_tindakan')[$i];
+                $qty = (int) $qty_temp;
+
+                $perhitungan = $harga_jual * $qty;
 
                 $sub_total = $sub_total + $perhitungan;
             }
